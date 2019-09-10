366850f837233e27bca2d9a678065d250f95834c
DarBeer <ki979rill@gmail.com>
2019-09-10 17:16:21 +0300

    adding routers and navigation

--- WatchNow/src/app/app-routing.module.ts
+++ WatchNow/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './web/home/home.component';
+import { MoviesComponent } from './web/movies/movies.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'movies', component: MoviesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WatchNow/src/app/app.module.ts
+++ WatchNow/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { FilmComponent } from './web/Movies/film/film.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
